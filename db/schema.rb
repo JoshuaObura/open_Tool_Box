b00d4fbb470669dbd1b937420b34e64da653cfe4
Daniela Sinclair <dasfluid@gmail.com>
2020-08-17 17:31:42 +0100

    Added references to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_145732) do
+ActiveRecord::Schema.define(version: 2020_08_17_161221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_08_17_145732) do
     t.date "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "tool_id", null: false
+    t.index ["tool_id"], name: "index_bookings_on_tool_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
@@ -43,5 +45,6 @@ ActiveRecord::Schema.define(version: 2020_08_17_145732) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "tools"
   add_foreign_key "bookings", "users"
 end
