05e59be5057903ca2e0d272c4ba5ca9cfbe4d2f1
Jess Pring <jessicapring@outlook.com>
2020-08-18 15:00:21 +0100

    seed fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_18_121540) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,10 +31,9 @@ ActiveRecord::Schema.define(version: 2020_08_18_121540) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "description"
-    t.string "category"
     t.bigint "user_id", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_tools_on_user_id"
-
   end
 
   create_table "users", force: :cascade do |t|
