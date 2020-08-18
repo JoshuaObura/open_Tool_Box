b2222a325599e1018432f496aac702e5365f2648
Daniela Sinclair <dasfluid@gmail.com>
2020-08-18 15:45:03 +0100

    Login details added

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
     resources :bookings, only: [ :new, :create ]
   end
   resources :bookings, only: [ :delete, :index, :show ]
-  resources :user, only: [ :show ]
+  resources :users, only: [ :show ]
  end
