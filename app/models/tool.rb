84058773825ac6752b0c43bb1717468c8a73f34c
jh <oluashegun@googlemail.com>
2020-08-18 14:45:23 +0100

    adding a catergory validation

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -3,4 +3,5 @@ class Tool < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :description, presence: true
+  validates :category, presence: true, inclusion: {in: ["home improvement", "automotive", "gardening", "cleaning"]}
 end
